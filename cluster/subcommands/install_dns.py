9aa29859aaef3de4fbb0dfae9f90ae60aa344aab
Louis Reid <louis-reid@stirlingbridge.website>
2023-12-15 12:14:23 -0700

    Some code

--- cluster/subcommands/install_dns.py
+++ cluster/subcommands/install_dns.py
@@ -6,9 +6,14 @@ from cluster.k8s_connection import Connection
 
 
 @click.command(help="Install external DNS service")
+@click.option("--do-token", required=True, help="Use this token for the Digitalocean DNS API")
+@click.option("--target-zone", required=True, help="Manage this DNS zone")
 @click.pass_context
-def command(context):
+def command(context, do_token, target_zone):
     command_context: MainCmdCtx = context.obj
     connection = Connection(command_context.config)
     connection.connect()
-    print("Hello DNS")
+    # Read the external-dns object definition YAML into memory
+    # Replace the template strings with our argumnents
+    # Send the YAML to k8s
+
